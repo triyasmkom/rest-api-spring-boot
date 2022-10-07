6dc10e155db6fc486ad4f456b72d46bff2341983
triyasmkom <triyavian@gmail.com>
2022-10-07 16:24:54 +0700

    implementasi soft delete

--- src/main/java/com/ths/restapi/entity/Product.java
+++ src/main/java/com/ths/restapi/entity/Product.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerator;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
@@ -18,6 +20,8 @@ import java.util.Set;
         generator = ObjectIdGenerators.PropertyGenerator.class,
         property = "id"
 )
+@SQLDelete(sql="UPDATE tbl_product SET is_deleted=true WHERE id=?")
+@Where(clause = "is_deleted=false")
 public class Product implements Serializable {
 
     @Id
@@ -45,5 +49,8 @@ public class Product implements Serializable {
     )
     //@JsonManagedReference
     private Set<Supplier> suppliers;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted=Boolean.FALSE;
 
 }
